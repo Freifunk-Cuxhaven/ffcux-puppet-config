4533128e5f384c695facc075bceda1db66a6a9d0
Borsal <info@host-base.email>
2019-05-07 21:05:04 +0200

    Update pre-puppet.sh

--- pre-puppet.sh
+++ pre-puppet.sh
@@ -34,7 +34,7 @@ hostname $HOST_PREFIX$VPN_NUMBER
 #echo "127.0.1.1 $SUBDOMAIN_PREFIX$VPN_NUMBER.$DOMAIN $HOST_PREFIX$VPN_NUMBER" >>/etc/hosts
 rm /etc/hostname
 touch /etc/hostname
-echo "$VPN_NUMBER$HOST_PREFIX" >>/etc/hostname
+echo "$HOST_PREFIX$VPN_NUMBER" >>/etc/hostname
 
 # install needed packages
 apt-get -y install sudo apt-transport-https git nload
